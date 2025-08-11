9b2b9bd48142648d4b976be564dd7d6eb64c239c
LushArmor20 <142264294+LushArmor20@users.noreply.github.com>
2025-08-11 18:25:26 +0100

    'falling' trees
    🍐

--- gambling/Scenes/Tree/tree.gd
+++ gambling/Scenes/Tree/tree.gd
@@ -3,6 +3,7 @@ extends Area3D
 @onready var coll_shape: CollisionShape3D = $CollisionShape3D
 @onready var dead_tree: MeshInstance3D = $dead_tree
 @onready var tree = tree_type.instantiate()
+@onready var original_y = tree.position.y
 @export var tree_type : PackedScene
 @export var max_health = 100
 @export var regrow_timer_sec = 30
@@ -10,6 +11,7 @@ extends Area3D
 var in_view = false
 var in_zone = false
 var health : int
+var move_down = false
 
 func _ready() -> void:
 	add_child(tree)
@@ -18,6 +20,13 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if  in_zone and in_view and Input.is_action_just_pressed("dig"):
 		mine_tree()
+
+	if move_down:
+		tree.position.y = lerp(tree.position.y, original_y - 7.5, 0.03)
+	if tree.position.y < original_y - 6:
+		tree.position.y = original_y - 2.0
+		tree.visible = false
+		move_down = false
 
 func _on_body_entered(body: Node3D) -> void:
 	if body is Player:
@@ -41,10 +50,12 @@ func mine_tree():
 
 func dead():
 	dead_tree.visible = true
-	tree.visible = false
+	move_down = true
 	coll_shape.disabled = true
 	await get_tree().create_timer(regrow_timer_sec).timeout
 	dead_tree.visible = false
-	tree.visible = true
 	coll_shape.disabled = false
 	health = max_health
+	move_down = false
+	tree.position.y = original_y
+	tree.visible = true
