f95efee064974147434b02b97abd5545030f01da
sourcelocation <52459150+sourcelocation@users.noreply.github.com>
2025-08-12 14:19:10 -0400

    amplify sound

--- gambling/Scenes/Menu/menu.gd
+++ gambling/Scenes/Menu/menu.gd
@@ -2,12 +2,12 @@ extends Control
 
 @export var game_scene : PackedScene
 @onready var game = game_scene.instantiate()
-@onready var option_sounds = [
-	preload("res://Scenes/Menu/nope1.mp3"),
-	preload("res://Scenes/Menu/nope2.mp3"),
-	preload("res://Scenes/Menu/nope3.mp3"),
-	preload("res://Scenes/Menu/nope4.mp3")
-]
+#@onready var option_sounds = [
+	#preload("res://Scenes/Menu/nope1.mp3"),
+	#preload("res://Scenes/Menu/nope2.mp3"),
+	#preload("res://Scenes/Menu/nope3.mp3"),
+	#preload("res://Scenes/Menu/nope4.mp3")
+#]
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Quit"):
@@ -21,11 +21,11 @@ func _on_play_pressed() -> void:
 	$UI.visible = false
 
 func _on_options_pressed() -> void:
-	if $AudioStreamPlayer != null:
-		var random_sound = option_sounds.pick_random()
-		if $AudioStreamPlayer.stream != random_sound:
-			$AudioStreamPlayer.stream = random_sound
-		$AudioStreamPlayer.play()
+	#if $AudioStreamPlayer != null:
+		#var random_sound = option_sounds.pick_random()
+		#if $AudioStreamPlayer.stream != random_sound:
+			#$AudioStreamPlayer.stream = random_sound
+	$AudioStreamPlayer.play()
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
